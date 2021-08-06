d10900d1c2c26ba7237f8bb87816517e9431aeed
K-ita <kazzerduck@gmail.com>
2021-08-06 23:24:20 +0300

    Changes in file database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE newtodo;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users(
   user_id UUID DEFAULT uuid_generate_v4(),
   user_name VARCHAR(255) NOT NULL,
@@ -17,48 +18,3 @@ CREATE TABLE todos(
   PRIMARY KEY (todo_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-INSERT INTO
-  users (
-    user_name,
-    user_email,
-    user_number,
-    user_address,
-    user_password
-  )
-VALUES
-  (
-    'duckkazzer',
-    'kazzerduck@gmail.com',
-    '+375333038098',
-    'Brestkih Diviziy dom 2 kvartira 5',
-    'time1999'
-  );
-INSERT INTO
-  users (
-    user_name,
-    user_email,
-    user_number,
-    user_address,
-    user_password
-  )
-VALUES
-  (
-    'Yoshi',
-    'yoshiworld@gmail.com',
-    '+375666777666',
-    'Nintendo',
-    'yahoo'
-  );
-INSERT INTO
-  todos(user_id, description)
-VALUES
-  (
-    '190daba6-02e9-42b7-9527-16f52057e346',
-    'PLAY MGS3:Snake Eater'
-  );
-UPDATE
-  users
-SET
-  role = 'admin'
-WHERE
-  user_name = 'duckkazzer';
