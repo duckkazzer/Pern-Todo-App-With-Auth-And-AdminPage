465df10bdf26685b86c52028003eedfbbbed2d33
K-ita <kazzerduck@gmail.com>
2021-08-08 13:09:01 +0300

    Add Confirm Password Input

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -8,9 +8,10 @@ const Register = ({ setAuth }) => {
     number: "",
     address: "",
     password: "",
+    password2: "",
   });
 
-  const { name, email, number, address, password } = inputs;
+  const { name, email, number, address, password, password2 } = inputs;
 
   const onChange = (e) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
@@ -19,16 +20,23 @@ const Register = ({ setAuth }) => {
   const onSubmitForm = async (e) => {
     e.preventDefault();
     try {
-      const body = { name, email, number, address, password };
+      if (password !== password2) {
+        const passwordCheckText = document.getElementById("CheckPasswordMatch");
+        document.getElementById("Password2").style.borderColor = "#B22222";
+        passwordCheckText.innerHTML = "Password does not match !";
+        passwordCheckText.style.color = "#B22222";
+      } else {
+        const body = { name, email, number, address, password };
 
-      const response = await fetch("http://localhost:5000/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
-      const parseRes = await response.json();
-      localStorage.setItem("token", parseRes.token);
-      setAuth(true);
+        const response = await fetch("http://localhost:5000/auth/register", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        });
+        const parseRes = await response.json();
+        localStorage.setItem("token", parseRes.token);
+        setAuth(true);
+      }
     } catch (error) {
       console.error(error.message);
     }
@@ -37,11 +45,11 @@ const Register = ({ setAuth }) => {
   return (
     <Fragment>
       <h1 className="text-center my-5">Register</h1>
-      <form onSubmit={onSubmitForm}>
+      <form id="identicalForm" onSubmit={onSubmitForm}>
         <input
           type="email"
           name="email"
-          placeholder="email"
+          placeholder="Email"
           value={email}
           className="form-control my-3"
           required
@@ -50,7 +58,7 @@ const Register = ({ setAuth }) => {
         <input
           type="text"
           name="name"
-          placeholder="name"
+          placeholder="Name"
           value={name}
           className="form-control my-3"
           required
@@ -59,7 +67,7 @@ const Register = ({ setAuth }) => {
         <input
           type="text"
           name="number"
-          placeholder="phone number"
+          placeholder="Phone number"
           value={number}
           className="form-control my-3"
           required
@@ -68,23 +76,39 @@ const Register = ({ setAuth }) => {
         <input
           type="text"
           name="address"
-          placeholder="address"
+          placeholder="Address"
           value={address}
           className="form-control my-3"
           required
           onChange={(e) => onChange(e)}
         />
         <input
+          required
+          id="Password"
           type="password"
           name="password"
-          placeholder="password"
+          placeholder="Password"
           value={password}
           className="form-control my-3"
-          required
           onChange={(e) => onChange(e)}
         />
-        <button className="btn btn-success btn-block">Register</button>
+        <div className="my-3">
+          <input
+            type="password"
+            id="Password2"
+            name="password2"
+            placeholder="Confirm Password"
+            value={password2}
+            className="form-control"
+            onChange={(e) => onChange(e)}
+          />
+          <div id="CheckPasswordMatch"></div>
+        </div>
+        <button type="submit" className="btn btn-success btn-block">
+          Register
+        </button>
       </form>
+
       <Link to="/login">Login</Link>
     </Fragment>
   );
